f8efbc0d208e1b08b3d1c87b4e83e0246d579c77
Oluwaponire <kponire@gmail.com>
2023-10-13 17:56:14 -0700

    printf

---
+++ _prinf.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+
+int _printf(char const *format, ...)
+{
+	int sum = 0;
+	int (*parameter)(va_list);
+	va_list pfList;
+
+	if (format == NULL)
+		return (-1);
+	va_start(pfList, format);
+
+	whille (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == '\0')
+				return (-1);
+			parameter = converter(*format);
+			sum += parameter;
+		}
+		else
+		{
+			_putchar(*format);
+			sum++;
+		}
+		format++;
+	}
+	va_end(pfList);
+	return (sum);
+}
