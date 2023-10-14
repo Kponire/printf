e537a1c09a36caf42de77a3f25c30031fc2e3884
Oluwaponire <kponire@gmail.com>
2023-10-13 20:07:15 -0700

    {

---
+++ print_decimal.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_decimal - a function that prints a decimal from the pfList
+ * when it encounters a %d specifier
+ * @pfList: the input decimal from the pfList
+ * Return: the length of the decimal number
+ */
+
+int print_decimal(va_list pfList)
+{
+	int number;
+	int sum = 0, x = 1;
+
+	number = va_arg(pfList, int);
+	if (number < 0)
+	{
+		number *= -1;
+		_putchar('-');
+		count++;
+	}
+	while ((number / x) > 9)
+		x *= 10;
+	while (x >= 1)
+	{
+		_putchar('0' + ((number / x) % 10));
+		x /= 10;
+		count++;
+	}
+	return (count);
+}
---
+++ print_int.c
@@ -0,0 +1,27 @@
+#include "main.h"                                                                                   /**
+ * print_int - a function that prints an integer from the pfList
+ * when it encounters a %d specifier
+ * @pfList: the input integer from the pfList
+ * Return: the length of the integer
+ */
+
+int print_int(va_list pfList)
+{
+        int number;
+        int sum = 0, x = 1;
+
+        number = va_arg(pfList, int);
+        if (number < 0)
+        {                                                         number *= -1;                                     _putchar('-');
+                count++;
+        }
+        while ((number / x) > 9)
+                x *= 10;
+        while (x >= 1)
+	{
+		_putchar('0' + ((number / x) % 10));
+		x /= 10;
+		count++;
+	}
+	return (count);
+}
